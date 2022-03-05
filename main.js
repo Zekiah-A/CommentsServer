953ebc43a2badbc6175f62861da93aa6ad5f0b72
Zekiah-A <zekiahamoako@outlook.com>
2022-03-05 14:12:34 +0000

    Server side spam and invalid message protection, logging

--- main.js
+++ main.js
@@ -4,11 +4,19 @@ const http = require("http");
 const host = 'localhost';
 const port = 8000;
 
+const maxNameCharacters = 16;
+const maxEmailCharacters = 24;
+const maxMessageCharacters = 4096;
+
 const defaultContent =
 "<div class=\"comment\">\n" +
 "   <em>NAME said:</em>\n" +
 "   <p>CONTENT</p>\n" +
 "</div>\n";
+
+function isBlank(string) { //empty / whitespace strings
+    return (!string || /^\s*$/.test(string));
+}
 
 const requestListener = function (req, res) { //request (incoming) response (outgoing)
     res.setHeader("Content-Type", "text/html");
@@ -40,15 +48,34 @@ const requestListener = function (req, res) { //request (incoming) response (out
                     return;
                 }
 
+                //Generated ANSI escape codes from @https://ansi.gabebanks.net/ 
                 var commentObject = JSON.parse(body);
+                if (isBlank(commentObject.name)) { //Check for blank name
+                    commentObject.name = "Anonymous";
+                }
+                if (isBlank(commentObject.email) || !commentObject.email.includes("@")) { //Check for blank or invalid email 
+                    commentObject.email = "anonymous@anonymous.com";
+                }
+                if (isBlank(commentObject.message)) { //Check for blank message
+                    console.log("\033[90;49;3mEmpty comment detected, rejecting.\033[0m");
+                    return;
+                }
+
+                //Trim name, email and message to prevent spam.
+                commentObject.name = commentObject.name.substring(0, maxNameCharacters);
+                commentObject.email = commentObject.email.substring(0, maxEmailCharacters);
+                commentObject.message = commentObject.message.substring(0, maxMessageCharacters);
+
                 var bodyHTML = commentObject.message.replaceAll("\n", "<br>");
-                var newComments = defaultContent.replace("NAME", commentObject.name).replace("CONTENT", bodyHTML) + "\n" + data + "\n<!--" + commentObject.email + "\n-->";
+                var newComments = "<!--" + commentObject.email + "-->\n" + defaultContent.replace("NAME", commentObject.name).replace("CONTENT", bodyHTML) + "\n" + data;
 
                 fs.writeFile("comments.html", newComments, err => {
                     if (err) {
                         console.error(err);
                         return;
                     }
+
+                    console.log("\033[90;49;3mSucessfully added comment | " + req.socket.remoteAddress + " | " + commentObject.name + " | " + commentObject.email + " | " +  commentObject.message + "\033[0m");
                 })
             })    
         });
