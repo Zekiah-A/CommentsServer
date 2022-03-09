1703e40efd3f34570cb35db76889da7e2db46c3b
Zekiah-A <zekiahamoako@outlook.com>
2022-03-09 22:22:27 +0000

    Shell script logging and verbose output

--- auto_run.sh
+++ auto_run.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 while true
 do
-	echo "Autostarting webserver with HTTPS."
-	sudo node main.js
+	if [ $# -eq 0 ]
+	  	then
+			echo "[!] No arguments supplied, all server stdout will be redirected to file server.log"
+			echo "Autostarted webserver with HTTPS."
+			sudo node main.js > server.log 2>&1
+		else
+			echo "[!] Arguments supplied, defaulting to verbose console output"
+			echo "Autostarted webserver with HTTPS."
+			sudo node main.js
+	fi
 	sleep 1
 done
 
